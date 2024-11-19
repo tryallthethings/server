d0beeedc7395a21bfbbc38961f385497706f584e
Côme Chilliet <come.chilliet@nextcloud.com>
2024-11-19 11:38:29 +0100

    fix(files_sharing): Do not wrap password policy exception into a generic one
    Let the controller access the HintException and show the error to the user.
    
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- lib/private/Share20/Manager.php
+++ lib/private/Share20/Manager.php
@@ -98,7 +98,7 @@ class Manager implements IManager {
 	 * Verify if a password meets all requirements
 	 *
 	 * @param string $password
-	 * @throws \Exception
+	 * @throws HintException
 	 */
 	protected function verifyPassword($password) {
 		if ($password === null) {
@@ -111,11 +111,7 @@ class Manager implements IManager {
 		}
 
 		// Let others verify the password
-		try {
-			$this->dispatcher->dispatchTyped(new ValidatePasswordPolicyEvent($password));
-		} catch (HintException $e) {
-			throw new \Exception($e->getHint());
-		}
+		$this->dispatcher->dispatchTyped(new ValidatePasswordPolicyEvent($password));
 	}
 
 	/**
